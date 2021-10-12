ca94182348a98d2fc903c8b0c4c0046263099f88
meanjula <anjula.ghale@gmail.com>
2021-10-12 13:55:24 +0300

    added props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 
 import './App.css';
 
+
+const Box=(props)=>{
+  return(
+    <div className="box">
+      <div>
+        <h2>{props.name}</h2>
+        <p>Title: {props.title}</p>
+        <p>Age:{props.age}</p>
+      </div>
+    </div>
+  )
+}
+
 function App() {
   return (
-    <h1>Hello world</h1>);
+  <>
+    <header>
+    <h1>Logo</h1>
+    </header>
+    <main>
+      <Box name="Maria" title="CEO" age="29"/>
+      <Box name="Anjula" title="Developer" age="31"/>
+      <Box name="Mary" title="Designer" age="28"/>
+    </main>
+    <footer>
+    <p><small>Copyrights</small></p>
+    </footer>
+    </> 
+    
+    )
+   
 }
 export default App;
